85df4716fbc354c6a402a6e934c1c00741b72078
sunnycode07 <sunny@pragra.io>
2024-09-28 10:54:45 -0400

    loops

---
+++ src/sep28/Test.java
@@ -0,0 +1,49 @@
+package sep28;
+
+public class Test {
+    public static void main(String[] args) {
+
+
+        int x = 0;
+
+
+        do{
+            System.out.println(x);
+            x++;
+        } while (x < 10 );
+
+
+        // 1. sum of n natural numbers  ,
+        // 100  1 2 3 4  5....100
+
+
+        // 2. print product table of 5 or any number
+        /*
+           5 * 1 = 5
+           5 * 2 = 10
+           5 * 3 = 15
+
+            result = num1 * num2;
+
+           sout(   num1 + " * " + " num2 " + " =  "  + result )
+
+         */
+
+        // factorial ?   5! = 5 * 4 * 3 * 2 * 1
+
+
+          //  x
+
+          // i
+
+          // fact
+
+        // fact = fact * i;
+        //fact ++;
+
+
+
+
+
+    }
+}
---
+++ src/sep28/readme.md
@@ -0,0 +1,39 @@
+## Recap 
+
+switch statement ?
+
+while loop 
+
+
+
+
+int x = -1;
+while(x <= 1  && x >= 5){ // false 
+
+System.out.println("Hello");
+
+
+}
+
+String s = "hello"
+
+switch(s == "hello"){
+
+case 1 :
+System.out.println("Hello 1");
+break;
+
+case 2 :
+System.out.println("Hello 2");
+break;
+
+
+
+}
+
+1. compile time error 
+2. runtime errror 
+3. Hello1 
+4. Hello2 
+5. Hello1  Hello2
+
