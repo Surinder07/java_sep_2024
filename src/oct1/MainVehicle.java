d270907084dd74015f0983836a1d93f72d319c35
sunnycode07 <sunny@pragra.io>
2024-10-01 21:07:51 -0400

    methods

---
+++ src/oct1/MainVehicle.java
@@ -0,0 +1,32 @@
+package oct1;
+
+public class MainVehicle {
+    public static void main(String[] args) {
+        // create object of a class
+        Vehicle vehicle = new Vehicle();
+
+        //  calling a method... , returning double value
+        double totalPrice = vehicle.price();
+        System.out.println(totalPrice);
+
+         vehicle.brake();
+         vehicle.brake();
+
+    }
+}
+
+
+//remote - power button
+
+/*
+
+
+
+
+
+
+
+
+ */
+
+
---
+++ src/oct1/Vehicle.java
@@ -0,0 +1,81 @@
+package oct1;
+
+public class Vehicle {
+
+  /*  // member variables  , Hardcoded values
+    String brand = "Audi";
+    String color = "Black";
+    double price = 2323232.23;*/
+
+    String brand;
+    String color;
+    double price;
+
+    public void brake(){
+        System.out.println("Car can brake.....");
+        return;
+
+    }
+
+    // double is return type
+    public double price(){
+        double totalPrice = 100 + 200;
+        return totalPrice;
+    }
+
+    // Alok go and make tea ---->
+
+    // Alok go and make tea and give it back to me
+
+
+
+    /*
+
+    void ? no returned value
+
+    accessModifier returnType methodName(){
+
+    // statement
+    // return;
+
+    }
+
+
+     */
+
+
+
+
+    /*
+     method ?
+     set of block of statements,
+     behaviour
+
+     brake
+     run
+     start
+     stop
+     acceralate
+
+
+    Human ?
+    eat , sleep , walk, talk , speak
+
+     */
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
