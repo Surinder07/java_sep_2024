9e77ca4e0a2d452a77d24358da9389094510fecc
sunnycode07 <sunny@pragra.io>
2024-10-12 12:05:28 -0400

    2d array

---
+++ src/oct12/ArrayDim.java
@@ -0,0 +1,47 @@
+package oct12;
+
+public class ArrayDim {
+    public static void main(String[] args) {
+
+
+
+        int[][] arr = new int[3][3];
+        arr[0][0] = 10;
+        arr[0][1] = 20;
+        arr[0][2] = 30;
+        arr[1][0] = 40;
+        arr[1][1] = 50;
+        arr[1][2] = 60;
+        arr[2][0] = 70;
+        arr[2][1] = 80;
+        arr[2][2] = 90;
+
+       /* for (int i = 0; i <arr.length ; i++) {
+            for (int j = 0; j < ; j++) {
+
+            }
+        }
+*/
+
+
+        System.out.println(arr[0][0]);
+
+
+        // matrix addition
+
+
+
+        /*
+
+         1 2 3       10 20 30
+         4 5 6   +   40 50 60
+         7 8 9       70 80 90
+
+
+
+
+
+         */
+
+    }
+}
